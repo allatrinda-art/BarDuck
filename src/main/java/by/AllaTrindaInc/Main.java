dda0a253f12f72452e49459bdf4abe145e7ade2f
allatrinda-art <alla.trinda@gmail.com>
2025-01-29 13:40:19 +0300

    1st commit

---
+++ src/main/java/by/AllaTrindaInc/Main.java
@@ -0,0 +1,9 @@
+package by.AllaTrindaInc;
+
+import org.testng.annotations.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
